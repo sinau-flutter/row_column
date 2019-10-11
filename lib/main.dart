ccb2d56124a9056504d586f6c0c9a5230c7d20a6
ayogatot <azerino25@gmail.com>
2019-10-11 11:13:02 +0700

    Row and Column

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Latihan Row Dan Column'),
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Ini Text 1'),
+            Text('Ini Text 2'),
+            Text('Ini Text 3'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Ini Text 4'),
+                Text('Ini Text 5'),
+                Text('Ini Text 6')
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
